ef54233032345c1b82c2a03da528372bdb31eada
Croquignoles <htavard@ensc.fr>
2025-02-04 19:14:15 +0100

    generate zip and publish

--- obj/Release/net8.0/colony-game.AssemblyInfo.cs
+++ obj/Release/net8.0/colony-game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("colony-game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e9eee9de1a9456d81cede2b632f26410e1aef1")]
 [assembly: System.Reflection.AssemblyProductAttribute("colony-game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("colony-game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
